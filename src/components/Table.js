b021c4c2b15978b690c68cfc4130ae56e5816a97
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-08-05 13:40:41 -0300

    Requirement seven

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { object } from 'prop-types';
 
 class Table extends Component {
   render() {
+    const three = 3;
+    const { expenses } = this.props;
     const tableDis = [
       'Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
       'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
+
     return (
       <table>
-        <tbody>
+        <thead>
           <tr>
             { tableDis.map((item) => <th key={ item }>{ item }</th>) }
           </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{parseFloat(expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>
+                {Math.round(
+                  Number(expense.exchangeRates[expense.currency].ask) * 100,
+                ) / 100}
+
+              </td>
+              <td>
+                {
+                  (parseFloat(expense.value)
+                    * parseFloat(expense.exchangeRates[expense.currency].ask)
+                  ).toFixed(three)
+                }
+              </td>
+              <td>Real</td>
+            </tr>
+          )) }
         </tbody>
       </table>
     );
   }
 }
 
-export default connect()(Table);
+Table.propTypes = {
+  expense: object,
+}.isRequired;
+
+const mapStateToProps = ({ wallet: { expenses } }) => ({
+  expenses,
+});
+
+export default connect(mapStateToProps)(Table);
